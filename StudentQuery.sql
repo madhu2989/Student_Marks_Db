b88a033bb27e82b3a5b1215c92073e7d7ee0e081
MadhuSudhan <51781807+madhu2989@users.noreply.github.com>
2020-11-28 13:46:53 +0530

    Add files via upload

---
+++ StudentQuery.sql
@@ -0,0 +1,60 @@
+
+
+create Database StudentDetailsdb
+go
+use StudentDetailsdb
+
+create table Student 
+(
+FStudentID int ,
+FStudentName nvarchar(50),
+ FSubject nvarchar(20),
+ FMarks int
+)
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(2,'Savita','EC1',50);
+
+ insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(2,'Savita','EC2',55);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(2,'Savita','EC3',28);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(2,'Savita','EC4',30);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(2,'Savita','EC5',60);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(3,'Abhi','EC1',40);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(3,'Abhi','EC2',38);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(3,'Abhi','EC3',35);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(3,'Abhi','EC4',45);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(3,'Abhi','EC5',37);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(4,'Priya','EC1',60);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(4,'Priya','EC2',49);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(4,'Priya','EC3',26);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(4,'Priya','EC4',29);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(4,'Priya','EC5',44);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(5,'Shanthi','EC1',70);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(5,'Shanthi','EC2',19);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(5,'Shanthi','EC3',45);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(5,'Shanthi','EC4',44);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(5,'Shanthi','EC5',50);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(6,'Harish','EC1',60);
+
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(6,'Harish','EC2',64);
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(6,'Harish','EC3',26);
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(6,'Harish','EC4',28);
+insert into Student(FStudentID,FStudentName,FSubject,FMarks)values(6,'Harish','EC5',29);
